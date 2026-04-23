c26b4e182906b490db49eb013c0b3d55c701140c
cloudsolutiongmbh <84773107+cloudsolutiongmbh@users.noreply.github.com>
2026-04-23 08:47:50 +0200

    Create Language-CH.ps1

---
+++ Language-CH.ps1
@@ -0,0 +1,50 @@
+$Title = "Set-KeyboardLanguage"
+$host.UI.RawUI.WindowTitle = $Title
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+
+$env:APPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Roaming"
+$env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
+$Env:PSModulePath = $env:PSModulePath + ";C:\Program Files\WindowsPowerShell\Scripts"
+$env:Path = $env:Path + ";C:\Program Files\WindowsPowerShell\Scripts"
+
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set-KeyboardLanguage.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+# Import International Modul
+ipmo international
+
+Write-Host -ForegroundColor Green "Install language packs"
+#Install-Language de-de
+#Install-Language fr-fr
+#Install-Language en-us
+
+# Gebietsschema
+Set-WinSystemLocale de-CH
+
+# Sprache und Region, Setze auf de-CH (Switzerland 223)
+Set-WinHomeLocation -GeoId 0xDF
+
+Write-Host -ForegroundColor Green "Set keyboard language to de-CH"
+Start-Sleep -Seconds 5
+
+$LanguageList = Get-WinUserLanguageList
+
+$LanguageList.Add("de-CH")
+Set-WinUserLanguageList $LanguageList -Force
+
+Start-Sleep -Seconds 5
+
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'de-DE'))
+Set-WinUserLanguageList $LanguageList -Force
+
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'en-US'))
+Set-WinUserLanguageList $LanguageList -Force
+
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'fr-FR'))
+Set-WinUserLanguageList $LanguageList -Force
+
+Stop-Transcript
